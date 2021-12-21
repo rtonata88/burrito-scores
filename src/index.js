785790ab8d8d652e3f8e6346bee9d5da636fd270
rtonata88 <rtonata@gmail.com>
2021-12-21 23:07:50 +0200

    Added the website footer

--- src/index.js
+++ src/index.js
@@ -8,15 +8,31 @@ import { getSchedules } from "./schedule";
 
 const schedules = getSchedules();
 const displaySchedules = () => {
-  //const table = document.getElementById("table-scores");
+  const moviesContainer = document.getElementById("movies-container");
+  let container = "";
   schedules.then((schedule) => {
     schedule.forEach((item) => {
       let info = item._embedded;
-      console.log(info.show.name);
-      console.log(info.show.id);
+      console.log(info.show.image);
+      container += `<div class="col-md-3 col-sm-12">
+                        <div class="card shadow-sm">
+                            <img src="${info.show.image.medium}">
+                            <div class="card-body">
+                                <p class="text-sm">${info.show.name}</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">See more details</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Comment</button>
+                                    </div>
+                                    <small class="text-muted">9 likes</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+
       numberOfLikes(info.show.id);
     });
-    //table.innerHTML = tableScoreRow;
+    moviesContainer.innerHTML = container;
   });
 };
 
