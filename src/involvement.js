602841e3dfaa2ea46602d7535071d2d5d4ee01d1
vicmaburrito <vmaldaraca@gmail.com>
2021-12-21 19:36:23 -0600

    Create record comment

--- src/involvement.js
+++ src/involvement.js
@@ -11,6 +11,17 @@ export const recordLike = async (data) => {
   });
 };
 
+export const recordComment = async (newComment) => {
+  await fetch(commentsEndpoint(), {
+    method: 'POST',
+    body: JSON.stringify(newComment),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
 export const scheduleTitlesCounter = (schedule) => schedule.length;
 
 export const commentsCounter = (comments) => comments.length;
