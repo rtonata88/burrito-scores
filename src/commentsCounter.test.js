fb152888964c28d482a5df360e3455e03de3ee61
vicmaburrito <vmaldaraca@gmail.com>
2021-12-24 10:58:52 -0600

    Fix comments counter test

--- src/commentsCounter.test.js
+++ src/commentsCounter.test.js
@@ -1,24 +1,24 @@
 import commentCount from './commentsCounter.js';
 
-describe('test comment count function', () => {
-  test('test number of comments from the API', () => {
-    const comment = [
-      {
-        comment: 'Great',
-        creation_date: '2021-12-24',
-        username: 'burrito',
-      },
-      {
-        comment: 'Nice',
-        creation_date: '2021-12-24',
-        username: 'Manuel',
-      },
-      {
-        comment: 'Awesome',
-        creation_date: '2021-12-24',
-        username: 'richard',
-      },
-    ];
-    expect(commentCount(comment)).toBe(3);
-  });
+const comments = () => Promise.resolve([
+  {
+    comment: 'Great',
+    creation_date: '2021-12-24',
+    username: 'burrito',
+  },
+  {
+    comment: 'Nice',
+    creation_date: '2021-12-24',
+    username: 'Manuel',
+  },
+  {
+    comment: 'Awesome',
+    creation_date: '2021-12-24',
+    username: 'richard',
+  },
+]);
+
+test('should count comments', async () => {
+  const comment = await comments();
+  expect(commentCount(comment)).toBe(3);
 });
